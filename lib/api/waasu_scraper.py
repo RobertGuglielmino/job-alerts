d12d176c428128e6f622321cfc7ca64e3e2befee
Robert Guglielmino <robert.gugliel@gmail.com>
2024-11-27 16:27:54 -0500

    vague organization

---
+++ lib/api/waasu_scraper.py
@@ -0,0 +1,19 @@
+
+
+class WAASUScraper:
+    def __init__(self, query):
+        self.query = query
+
+    def get_companies(self):
+        # login w data
+
+        # call info
+
+        # etl 
+
+        # restrictions class?
+        filters, name, location,  is_hiring
+
+        # job_name, name,  about, job_url
+
+        # return
